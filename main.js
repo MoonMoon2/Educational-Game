aba13995a590743979c0b705ca9fb2f8bd16ac03
Tobias Ward <tate@tatesworld.com>
2018-02-15 17:49:47 +0100

    Clear out the old, beginning to create the new

--- main.js
+++ main.js
@@ -11,26 +11,26 @@ const ipcMain = electron.ipcMain;
 let mainWindow;
 
 function createWindow() {
-	// Create the browser window.
-	mainWindow = new BrowserWindow({
-		width: 900,
-		height: 475,
-		icon: __dirname + '/logo.png'
-	});
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 1000,
+        icon: __dirname + '/logo.png'
+    });
 
-	// and load the index.html of the app.
-	mainWindow.loadURL(`file://${__dirname}/index.html`);
+    // and load the index.html of the app.
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
 
     // Uncomment to open dev tools (Inspect Element) automatically
-	// mainWindow.webContents.openDevTools();
+    mainWindow.webContents.openDevTools();
 
-	// Emitted when the window is closed.
-	mainWindow.on('closed', function() {
-		// Dereference the window object, usually you would store windows
-		// in an array if your app supports multi windows, this is the time
-		// when you should delete the corresponding element.
-		mainWindow = null;
-	});
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function() {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
+    });
 }
 
 // This method will be called when Electron has finished
@@ -40,32 +40,34 @@ app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function() {
-	// On OS X it is common for applications and their menu bar
-	// to stay active until the user quits explicitly with Cmd + Q
-	if (process.platform !== 'darwin') app.quit();
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    // if (process.platform !== 'darwin') {
+    app.quit();
+    // }
 });
 
 app.on('activate', function() {
-	// On OS X it's common to re-create a window in the app when the
-	// dock icon is clicked and there are no other windows open.
-	if (mainWindow === null) createWindow();
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) createWindow();
 });
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
 ipcMain.on('renderData', function(event, arg) {
-	//setting dialog
-	dataWindow = new BrowserWindow({
-		width: 1000,
-		height: 500
-	});
-	// Load options page
-	dataWindow.loadURL(`file://${ __dirname}/data.html`);
+    //setting dialog
+    dataWindow = new BrowserWindow({
+        width: 1000,
+        height: 500
+    });
+    // Load options page
+    dataWindow.loadURL(`file://${ __dirname}/data.html`);
 
-	dataWindow.on('closed', function() {
-		// Dereference the window object, usually you would store windows
-		// in an array if your app supports multi windows, this is the time
-		// when you should delete the corresponding element.
-		dataWindow = null;
-	});
+    dataWindow.on('closed', function() {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        dataWindow = null;
+    });
 });
