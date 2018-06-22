3ae84f50fc55cf2b6b0d59104d12f5128740b34d
Tobias Ward <tate@tatesworld.com>
2018-06-22 12:22:35 +0200

    Tried three ways to show video, settled on one and began work on it.

--- js/game.js
+++ js/game.js
@@ -16,14 +16,9 @@ async function timeTaken(correct, tstart, tallow) {
     }
 }
 function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function mainGame(uname, pscore) {
-    if (fs.existsSync(localStorage.path) && fs.statSync(localStorage.path).size > 0) {
-        let data = render(JSON.parse(fs.readFileSync(localStorage.path)));
-    } else {
-        // Display "no data" warning if no data is found
-        warning.style.display = 'block';
-    }
+async function mainGame() {
+     
 }
